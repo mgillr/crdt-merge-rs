726685b58f1bdddfdb65e922fba5643f523e5866
Ryan Gillespie <36207807+mgillr@users.noreply.github.com>
2026-03-27 02:35:11 +0100

    Add copyright header

--- src/core.rs
+++ src/core.rs
@@ -1,3 +1,8 @@
+// Copyright 2026 Ryan Gillespie
+// SPDX-License-Identifier: Apache-2.0
+//
+// Commercial licensing: data@optitransfer.ch, rgillespie83@icloud.com
+
 //! Core CRDT types: GCounter, PNCounter, LWWRegister, ORSet.
 
 use serde::{Deserialize, Serialize};
