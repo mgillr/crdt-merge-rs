d2e91dd504c41aece60adc9b3c977d46f57e5f89
Ryan Gillespie <36207807+mgillr@users.noreply.github.com>
2026-03-27 02:35:12 +0100

    Add copyright header

--- src/json_merge.rs
+++ src/json_merge.rs
@@ -1,3 +1,8 @@
+// Copyright 2026 Ryan Gillespie
+// SPDX-License-Identifier: Apache-2.0
+//
+// Commercial licensing: data@optitransfer.ch, rgillespie83@icloud.com
+
 //! Deep JSON merge using LWW semantics.
 
 use crate::merge::Strategy;
